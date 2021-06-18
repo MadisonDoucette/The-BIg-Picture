1a9842347bf5e7911e10cb2a2b3a4cf0a07f9297
Madison Doucette <mdoucette@Madisons-MacBook-Air.local>
2021-06-18 12:12:20 -0700

    first commit

---
+++ TBP4.js
@@ -0,0 +1,95 @@
+let cards = [
+    {
+        "img":"beach.jpeg",
+        "text":"Old Orchard<br>05.20.21",
+        "info": "A solo picture with unique colour and perspective showing the moon directly overhead.",
+    },
+    {
+        "img":"Buntzen/B2.jpeg",
+        "text":"Buntzen Lake<br>02.26.21",
+        "info": "This photography series was taken during a hike to Buntzen Lake Power House 2.",
+        "url":"Buntzen.html",
+    },
+    {
+        "img":"petri.jpeg",
+        "text":"ILLIAD PROJECT<br>04.08.20",
+        "info": "A solo picture taken of tests 1 & 6 in the Iliad Project experiment.",
+
+    },
+    {
+        "img":"waterfall.jpeg",
+        "text":"SAWBLADE FALLS<br>04.18.21",
+        "info": "A solo picture taken of Sawblade Falls.",
+    },
+    {
+        "img":"sunset.jpeg",
+        "text":"Old Orchard<br>05.31.21",
+        "info": "A solo picture with the rule of thirds and unique colour."
+    },
+    {
+        "img":"arizona.jpeg",
+        "text":"Arizona Tea<br>04.09.21",
+        "info": "A solo picture taken to highlight the unique colours seen on the designs of Arizona cans."
+    },
+    {
+        "img":"Vancouver.jpeg",
+        "text":"Vancouver Art Museum<br>08.27.20",
+        "info": "An edited solo picture with the rule of thirds highlighting the 'Greek Revival' style of architechture seen in the Vancouver Art Museum."
+    },
+    {
+        "img":"lights.jpeg",
+        "text":"Rocky Point Pier<br>05.12.21",
+        "info": "A solo picture with the rule of thirds, experimenting with night-time photography."
+    },
+    {
+        "img":"BCWG.png",
+        "text":"BC Winter Games<br>02.19-24.20",
+        "info": "A photo of the Zone 4 BC Winter Games Ringette team. I played goalie for that team and had the opportunity to travel to Fort St. John and play against the best ringette teams in the province.",
+        "url":"BCWG.html"
+    },
+    {
+        "img":"Newman/Glenn_Gould2.png",
+        "text":"Arnorld Newman<br>11.12.20",
+        "info": "A series of photos inspired by famous portraits taken by Arnold Newman.",
+        "url":"Arnold_Newman.html"
+    },
+    {
+        "img":"album_art.jpeg",
+        "text":"Album Art<br>05.26.21",
+        "info": "I was asked to create art for the cover of Trevor Cuffs' podcast. The requests were for a busy background with a curved version of the Inquiry Hub logo wearing headphones in the middle."
+    },
+    {
+        "img":"explore_new_worlds.png",
+        "text":"Golden Circle<br>11.08.20",
+        "info": "A poster created as part of the Personal Golden Circle, highlighting my interest in reading.",
+        "url":"Explore_New_Worlds.html"
+    },
+    {
+        "img":"every_child_matters.png",
+        "text":"Orange Shirt Day<br>10.30.20",
+        "info": "This Poster was created to bring attention to Orange Shirt day and all Indigenous people affected by the Residential School system in Canada",
+        "url":"Every_Child_Matters.html"
+    },
+    {
+        "img":"mosaic.png",
+        "text":"Inquiry Hub Mosaic<br>11.05.20",
+        "info":"A collection of 16 individual posters that can be displayed in a 4x4 grid to create the Inquiry Hub logo.",
+        "url":"Mosaic.html"
+    },
+    {
+        "img":"DotD.png",
+        "text":"Day of the Dead<br>11.02.20",
+        "info":"A sugar skull created in honor of the Day of the Dead, a Mexican celebration that celebrates the dead."
+    },
+    {
+        "img":"salmon.jpeg",
+        "text":"Haida Salmon<br>11.20.19",
+        "info":"Part of a collection of 3, this salmon was craeated in the traditional Haida Gwaii art style."
+    },
+    {
+        "img":"sketches/space.jpeg",
+        "text":"My Sketchbook<br>11.05.20",
+        "info":"A collection of sketches, doodles and drawings taken from my sketchbook.",
+        "url":"sketches.html"
+    },
+];
